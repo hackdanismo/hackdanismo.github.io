82e02db947659056c90509af100808509e3028d2
hackdanismo <office@justtoconfirm.co.uk>
2025-02-10 20:24:45 +0000

    feat: add the initial CSS and JavaScript to add toggle button and functionality to apply dark mode to the page to toggle between light and dark modes

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+const toggleSwitch = document.getElementById("theme-toggle");
+
+// Check if the user has already selected a theme before
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    toggleSwitch.checked = true;
+}
+
+// Toggle the theme on checkbox change
+toggleSwitch.addEventListener("change", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // Store preference in localStorage
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+})
